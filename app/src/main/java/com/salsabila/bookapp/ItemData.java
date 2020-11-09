62420f0d6c2bb2a65e001fc7a2f243073c9ea6a5
salsabilalailyrahma95@gmail.com <salsabilalailyrahma95@gmail.com>
2020-11-09 12:43:15 +0700

    Initial commit

---
+++ app/src/main/java/com/salsabila/bookapp/ItemData.java
@@ -0,0 +1,8 @@
+package com.salsabila.bookapp;
+
+public class ItemData {
+    public static String itemTitle;
+    public static String itemAuthor;
+    public static String itemImage;
+    public static String itemDescription;
+}
